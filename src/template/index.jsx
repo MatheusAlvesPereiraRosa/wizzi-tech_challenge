d1952cd73a5cc011e125122223fa6a0bc262e571
Matheus Alves <matpr.a98@gmail.com>
2023-07-12 15:10:44 -0300

    Arrumando layout do formulário

--- src/template/index.jsx
+++ src/template/index.jsx
@@ -1,13 +1,25 @@
 // import logo from "../logo.svg";
 // import "./index.css";
-import { Header } from "../components/Header";
+import { useState } from "react";
+
+import { Form } from "../components/Form/index.jsx";
+// import { Header } from "../components/Header";
 
 function Home() {
+  const [form, setForm] = {
+    appartureDate: "",
+    deppartureDate: "",
+    adults: 0,
+    children: 0,
+    passagerName: "",
+    locationName: "",
+  };
+
   return (
-    <div className="fluid-container">
-      <Header />
-      <div className="container">
-        <h1>Formulário</h1>
+    <div className="bg-image position-relative vh-100">
+      <div className="filter" />
+      <div className="fluid-container vh-100 position-relative d-flex flex-column justify-content-center">
+        <Form form={form}/>
       </div>
     </div>
   );
