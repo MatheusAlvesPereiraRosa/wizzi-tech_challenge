4fe26c7dbefd975763c65fd88cf5d68d49e9dc84
Matheus Alves <matpr.a98@gmail.com>
2023-07-15 16:28:39 -0300

    Corrigindo layout e responsividade

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -58,7 +58,7 @@ export const Form = () => {
     }
 
     if (form.adults === "0") {
-      setErrorAdults("A passagem deve ser comprada para ao menos uma pessoa");
+      setErrorAdults("Necessário um passageiro ao menos");
       error = true;
     }
 
@@ -122,152 +122,157 @@ export const Form = () => {
 
   return (
     <form
-      className="form-box fluid-container row align-items-baseline margin-central p-3 rounded-3 my-auto "
+      className="p-3 bg-body-tertiary container rounded-3"
       onSubmit={handleSubmit}
       ref={formRef}
     >
+      <h2 className="text-center mb-3">Carbo-viagens</h2>
+
       {status.type === "sucesso" ? (
         <div className="alert alert-success" role="alert">
-          {status.message}
+          <span>{status.message}</span>
         </div>
       ) : (
         ""
       )}
       {status.type === "erro" ? (
         <div className="alert alert-danger" role="alert">
-          {status.message}
+          <span>{status.message}</span>
         </div>
       ) : (
         ""
       )}
-      <div className="col-lg-3 col-md-6">
-        <label className="form-label">Ida:</label>
-        <input
-          type="date"
-          className="form-control"
-          name="departure"
-          max="9999-12-31"
-          value={form.departure}
-          onChange={(e) => {
-            handleChange(e), setErrorDeparture(null);
-          }}
-        />
-        {errorDeparture && (
-          <span className="text-danger fw-bolder">{errorDeparture}</span>
-        )}
+
+      <div className="input-container form-scroll fluid-container row align-items-baseline">
+        <div className="col-lg-3 col-md-6">
+          <label className="form-label">Ida:</label>
+          <input
+            type="date"
+            className="form-control"
+            name="departure"
+            max="9999-12-31"
+            value={form.departure}
+            onChange={(e) => {
+              handleChange(e), setErrorDeparture(null);
+            }}
+          />
+          {errorDeparture && (
+            <span className="text-danger fw-bolder">{errorDeparture}</span>
+          )}
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <label className="form-label">Volta:</label>
+          <input
+            type="date"
+            className="form-control"
+            name="return"
+            max="9999-12-31"
+            value={form.return}
+            onChange={(e) => {
+              handleChange(e), setErrorReturn(null);
+            }}
+          />
+          {errorReturn && (
+            <span className="text-danger fw-bolder">{errorReturn}</span>
+          )}
+        </div>
+        <div className="col-lg-3  col-md-6 mt-3">
+          <label className="form-label">
+            Quantidade de passageiros (adultos):
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            name="adults"
+            min={0}
+            value={form.adults}
+            onChange={(e) => {
+              handleChange(e), setErrorAdults(null);
+            }}
+          />
+          {errorAdults && (
+            <span className="text-danger fw-bolder">{errorAdults}</span>
+          )}
+        </div>
+        <div className="col-lg-3 col-md-6 mt-3">
+          <label className="form-label">
+            Quantidade de passageiros (crianças):
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            name="children"
+            min={0}
+            value={form.children}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col-md-6 mt-3">
+          <label className="form-label">Origem</label>
+          <input
+            type="name"
+            className="form-control"
+            placeholder="Carbonita"
+            name="origin"
+            value={form.origin}
+            onChange={(e) => {
+              handleChange(e), setErrorOrigin(null);
+            }}
+          />
+          {errorOrigin && (
+            <span className="text-danger fw-bolder">{errorOrigin}</span>
+          )}
+        </div>
+        <div className="col-md-6 mt-3">
+          <label className="form-label">Destino</label>
+          <input
+            type="name"
+            className="form-control"
+            placeholder="Belo Horizonte"
+            name="destination"
+            value={form.destination}
+            onChange={(e) => {
+              handleChange(e), setErrorDestination(null);
+            }}
+          />
+          {errorDestination && (
+            <span className="text-danger fw-bolder">{errorDestination}</span>
+          )}
+        </div>
+        <div className="col-md-6 mt-3">
+          <label className="form-label">Nome do passageiro principal</label>
+          <input
+            type="name"
+            className="form-control"
+            name="name"
+            placeholder="Seu nome"
+            value={form.name}
+            onChange={(e) => {
+              handleChange(e), setErrorName(null);
+            }}
+          />
+          {errorName && (
+            <span className="text-danger fw-bolder">{errorName}</span>
+          )}
+        </div>
+        <div className="col-md-6 mt-3">
+          <label className="form-label">Endereço de email</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            placeholder="algo@exemplo.com"
+            value={form.email}
+            onChange={(e) => {
+              handleChange(e), setErrorEmail(null);
+            }}
+          />
+          {errorEmail && (
+            <span className="text-danger fw-bolder">{errorEmail}</span>
+          )}
+        </div>
       </div>
-      <div className="col-lg-3 col-md-6">
-        <label className="form-label">Volta:</label>
-        <input
-          type="date"
-          className="form-control"
-          name="return"
-          max="9999-12-31"
-          value={form.return}
-          onChange={(e) => {
-            handleChange(e), setErrorReturn(null);
-          }}
-        />
-        {errorReturn && (
-          <span className="text-danger fw-bolder">{errorReturn}</span>
-        )}
-      </div>
-      <div className="col-lg-3  col-md-6 mt-3">
-        <label className="form-label">
-          Quantidade de passageiros (adultos):
-        </label>
-        <input
-          type="number"
-          className="form-control"
-          name="adults"
-          min={0}
-          value={form.adults}
-          onChange={(e) => {
-            handleChange(e), setErrorAdults(null);
-          }}
-        />
-        {errorAdults && (
-          <span className="text-danger fw-bolder">{errorAdults}</span>
-        )}
-      </div>
-      <div className="col-lg-3 col-md-6 mt-3">
-        <label className="form-label">
-          Quantidade de passageiros (crianças):
-        </label>
-        <input
-          type="number"
-          className="form-control"
-          name="children"
-          min={0}
-          value={form.children}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="col-md-6 mt-3">
-        <label className="form-label">Origem</label>
-        <input
-          type="name"
-          className="form-control"
-          placeholder="Carbonita"
-          name="origin"
-          value={form.origin}
-          onChange={(e) => {
-            handleChange(e), setErrorOrigin(null);
-          }}
-        />
-        {errorOrigin && (
-          <span className="text-danger fw-bolder">{errorOrigin}</span>
-        )}
-      </div>
-      <div className="col-md-6 mt-3">
-        <label className="form-label">Destino</label>
-        <input
-          type="name"
-          className="form-control"
-          placeholder="Belo Horizonte"
-          name="destination"
-          value={form.destination}
-          onChange={(e) => {
-            handleChange(e), setErrorDestination(null);
-          }}
-        />
-        {errorDestination && (
-          <span className="text-danger fw-bolder">{errorDestination}</span>
-        )}
-      </div>
-      <div className="col-md-6 mt-3">
-        <label className="form-label">Nome do passageiro principal</label>
-        <input
-          type="name"
-          className="form-control"
-          name="name"
-          placeholder="Seu nome"
-          value={form.name}
-          onChange={(e) => {
-            handleChange(e), setErrorName(null);
-          }}
-        />
-        {errorName && (
-          <span className="text-danger fw-bolder">{errorName}</span>
-        )}
-      </div>
-      <div className="col-md-6 mt-3">
-        <label className="form-label">Endereço de email</label>
-        <input
-          type="email"
-          className="form-control"
-          name="email"
-          placeholder="algo@exemplo.com"
-          value={form.email}
-          onChange={(e) => {
-            handleChange(e), setErrorEmail(null);
-          }}
-        />
-        {errorEmail && (
-          <span className="text-danger fw-bolder">{errorEmail}</span>
-        )}
-      </div>
-      <div className="d-grid gap-2 mt-3">
+      <div className="d-grid mt-3">
         <button className="btn btn-primary" type="submit">
           Enviar
         </button>
--- src/template/index.jsx
+++ src/template/index.jsx
@@ -1,16 +1,15 @@
 // import logo from "../logo.svg";
-// import "./index.css";
+import "./index.css";
 import { Form } from "../components/Form/index.jsx";
 // import { Header } from "../components/Header";
 
 function Home() {
   return (
-    <>
-      <div className="filter opacity-100 position-absolute bottom-0 start-0 w-100 h-100" />
-      <div className="position-relative fluid-container h-100 position-relative d-flex flex-column justify-content-center vh-100">
+    <main>
+      <div className="position-absolute top-50 start-50 translate-middle form-container">
         <Form />
       </div>
-    </>
+    </main>
   );
 }
 
